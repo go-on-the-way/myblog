28463e03a5132387098295e30e7b6dbac9986079
wanglei1003 <245693023@qq.com>
2015-06-09 21:46:53 +0800

    wl

--- javascripts/index.js
+++ javascripts/index.js
@@ -4,4 +4,10 @@ window.onload = function(){
 	var containerEle = document.getElementById("container");
 	containerEle.style.width = width+"px";
 	containerEle.style.height = height+"px";
+	
+	var photoParent  = document.getElementById("photoalbum");
+	photoParentWidth = photoParent.style.width||photoParent.style.offsetWidth;
+	var photoElement =document.getElementById("photo");
+	photoElement.style.left = (photoParentWidth - 100)+"px";
+	var photos = [];
 };
